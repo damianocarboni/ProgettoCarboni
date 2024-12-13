4e1095c3a4437dbaec7cf052df9905dbd2c71f31
damianocarboni <carboni.damiano@itismeucci.com>
2024-12-13 10:23:23 +0100

    Setup iniziale progetto

---
+++ stampa.cpp
@@ -0,0 +1,19 @@
+/*
+Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
+
+Damiano Carboni 13/12/2024
+*/
+
+# include <iostream>
+using namespace std;
+int main()
+{
+  int i=10 ,n;
+  cout<<"Quante volte voi stampare la frase? ";
+  cin>>n;
+  while (i<n)
+  {
+   cout<<"Bada come la fuma"<<endl; 
+  }
+  system("pause");
+}
